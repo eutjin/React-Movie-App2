948230eeb85954aa431659874ff02173e2856ee6
eutjin <91615591+eutjin@users.noreply.github.com>
2022-08-12 01:08:29 +0900

    deploy 2

--- frontend/src/BaseUrl.js
+++ frontend/src/BaseUrl.js
@@ -1,4 +1,4 @@
 const baseUrl = process.env.NODE_ENV === "production"
-? "https://clownfish-app-6fhry.ondigitalocean.app"
+? "https://shark-app-veod2.ondigitalocean.app"
 : "http://localhost:5000";
 export default baseUrl;
